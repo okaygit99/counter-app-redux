dd9b5fdea2dfd39f26065f5d0c768877ad4d7bff
Okay <okayeverybody6@gmail.com>
2025-06-12 21:24:55 +0530

    Counter App using react redux

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement, reset, incrementByAmount } from './features/counter/CounterSlice'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const count=useSelector((state)=> state.counter.value);
+  const dispatch = useDispatch();
+  const [amount, setAmount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{textAlign: 'center', marginTop:"5 rem"}}>
+      <h1>Redux Counter</h1>
+      <h2>{count}</h2>
+      <div style={{margin: '1rem'}}>
+      <button onClick={()=> dispatch(increment())}>Increment</button>{''}
+      <button onClick={()=> dispatch(decrement())}>Decrement</button>{''}
+      <button onClick={()=> dispatch(reset())}>Reset</button>{''}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div style={{margin:'1rem'}}>
+        <input type="number" value={amount} onChange={(e)=> setAmount(Number(e.target.value))} placeholder='Enter Amount'/>
+
+        <button onClick={()=> dispatch(incrementByAmount(amount))}>Add Amount</button>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
